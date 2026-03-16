29ec33d8b41f018ff67e4467b8e31e160372aa89
Hor1e <romandementev929@gmail.com>
2026-03-16 09:54:03 +0500

    В целом этот кусок кода доделан. Треки сохраняются в папку.

--- music.py
+++ music.py
@@ -1,11 +1,15 @@
 #импорты
 from youtubesearchpython import VideosSearch as search
 import yt_dlp
-
+import os
 
 #Переменная чтоб найти
-idk = 'плачь - rizza'
-
+idk = f'кислород капсайз'
+#папка
+folder = 'tracks'
+#создание папки если ее нет
+if not os.path.exists(folder):
+    os.makedirs(folder)
 
 #создание класса
 class MusicManager:
@@ -17,11 +21,13 @@ class MusicManager:
         
     #метод для получения ссылки на видео
     def getlink(self):
+        
         music = search(self.name, limit=1)
         searchdict = music.result()
-        searchlist = searchdict['result']
+        self.link =  searchdict['result'][0]['link']
         #сохранение перемеенной в self, для обращения к ней в другом методе
-        self.link = searchlist[0]['link']
+        
+        print('Ссылка на скачивание: ', self.link)
         
         
         
@@ -31,23 +37,41 @@ class MusicManager:
         self.getlink()
         #настроечки для скачивания аудио
         ydl_opts = {
+            #настройки подключения
+            'source_address': '0.0.0.0', 
+            'http_chunk_size': 262144,  
+            'retries': 100,               
+            'extractor_args': {
+            'youtube': {
+            'player_client': ['android', 'web'], # Использовать разные типы клиентов
+            }
+            #настройка высасывания аудио
+            },
             'format': 'bestaudio/best',
             'postprocessors': [{
                 'key': 'FFmpegExtractAudio',
                 'preferredcodec': 'mp3',
                 'preferredquality': '192',
             }],
-            'outtmpl': '%(title)s.%(ext)s',
+            'outtmpl': f'{folder}/%(title)s.%(ext)s',
+                
         }
         #само скачивание
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             ydl.download([self.link])
+     
 
+
+        
+       
+        
+            
         
 
 
+
 idk2 = MusicManager(idk)
-idk2.getlink()
 idk2.downloadtrack()
+
 
 
